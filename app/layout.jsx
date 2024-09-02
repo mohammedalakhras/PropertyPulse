e52dc81590a6fe1d503f75dc72d6a82b5273af78
Mohammed Alakhras <MabdMAk@gmail.com>
2024-09-02 09:03:12 +0300

    First Commit

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import '@/assets/styles/globals.css';
+
+
+export const metadata={
+    title:'PropertyPulse | Find The Perfect Rental',
+    description:'Find your dream rental property',
+    keywords:'rental, find rental, find properties,  '
+}
+export default function layout({children}) {
+  return (
+    <html lang='en'>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
